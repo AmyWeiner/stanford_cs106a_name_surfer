9d4193edbb02d1827952da74064a6bf0477c4763
S <s>
2012-05-23 22:39:23 -0700

    {"type":"TI", "runs":"1"}

--- NameSurferGraph.java
+++ NameSurferGraph.java
@@ -10,40 +10,62 @@ import java.awt.*;
 import java.util.*;
 
 public class NameSurferGraph extends ResizableGCanvas
-                             implements NameSurferConstants {
+implements NameSurferConstants {
 
-/**
- * Creates a new NameSurferGraph object that displays the data.
- */
+	/**
+	 * Creates a new NameSurferGraph object that displays the data.
+	 */
 	public NameSurferGraph() {
-		 
+
 	}
 
-/**
- * Clears the list of name surfer entries stored inside this class.
- */
+	/**
+	 * Clears the list of name surfer entries stored inside this class.
+	 */
 	public void clear() {
 		// You fill this in
 	}
 
-/**
- * Adds a new NameSurferEntry to the list of entries on the display.
- * Note that this method does not actually draw the graph, but
- * simply stores the entry; the graph is drawn by calling update.
- */
+	/**
+	 * Adds a new NameSurferEntry to the list of entries on the display.
+	 * Note that this method does not actually draw the graph, but
+	 * simply stores the entry; the graph is drawn by calling update.
+	 */
 	public void addEntry(NameSurferEntry entry) {
 		// You fill this in
 	}
 
-/**
- * Updates the display image by deleting all the graphical objects from
- * the canvas and then reassembling the display according to the list
- * of entries.  Your application must call update after calling either
- * clear or addEntry; update is also called automatically whenever the
- * size of the canvas changes.
- */
+	/**
+	 * Updates the display image by deleting all the graphical objects from
+	 * the canvas and then reassembling the display according to the list
+	 * of entries.  Your application must call update after calling either
+	 * clear or addEntry; update is also called automatically whenever the
+	 * size of the canvas changes.
+	 */
 	public void update() {
-		// You fill this in
+		drawMargins();
 	}
 
+	private void drawMargins() {
+		drawTopMargins();
+		drawBottomMargins();
+	}
+
+	private void drawTopMargins() {
+		int xUpperLeft = 0;
+		int xUpperRight = getWidth();
+		int yUpper = GRAPH_MARGIN_SIZE;
+		GLine line = new GLine(xUpperLeft, yUpper, xUpperRight, yUpper);
+		add(line);
+	}
+	
+	private void drawBottomMargins() {
+		int xLowerLeft = 0;
+		int xLowerRight = getWidth();
+		int yLower = getHeight() - GRAPH_MARGIN_SIZE;
+		GLine line = new GLine(xLowerLeft, yLower, xLowerRight, yLower);
+		add(line);
+	}
 }
+
+
