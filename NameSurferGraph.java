1661026e0b937dd2fb29b0c7e002d57fa7929a71
S <s>
2012-05-25 08:02:21 -0700

    {"type":"TI", "runs":"2"}

--- NameSurferGraph.java
+++ NameSurferGraph.java
@@ -104,7 +104,7 @@ implements NameSurferConstants {
 				if (rank < GRAPH_MARGIN_SIZE) {
 					rank = GRAPH_MARGIN_SIZE;
 				} else if (rank == 0) {
-					rank = getHeight() - GRAPH_MARGIN_SIZE;
+					rank = 200;
 				}
 				GLabel label = new GLabel(name + " " + rank, j * separation, rank);
 				add(label);
