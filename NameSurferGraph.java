05fd9664d9daa56c8b69708a576b25b3f0a2bf18
S <s>
2012-05-24 21:24:14 -0700

    {"type":"TI", "runs":"1"}

--- NameSurferGraph.java
+++ NameSurferGraph.java
@@ -128,11 +128,17 @@ implements NameSurferConstants {
 		for (int i = 0; i < points.size(); i ++) {
 			double x1 = points.get(i).getX();
 			double y1 = points.get(i).getY();
+			if (y1 == 0) {
+				y1 = getHeight();
+			}
 			double x2 = points.get(i + 1).getX();
 			double y2 = points.get(i + 1).getY();
+			if (y2 == 0) {
+				y2 = getHeight();
+			}
 			GLine line = new GLine(x1, y1, x2, y2);
 			add(line);
-			//line.setColor(colorGraph(c));
+			line.setColor(colorGraph(c));
 		}
 	}
 
